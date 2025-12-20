84c0f15ecf64e930cf341bd6c2f5e131dc75c31a
Salvo Giangreco <giangrecosalvo9@gmail.com>
2025-12-20 22:17:52 +0800

    a71: init
    Co-authored-by: Samuel Pascua <pascua.samuel.14@gmail.com>
    Signed-off-by: Samuel Pascua <pascua.samuel.14@gmail.com>

---
+++ target/a71/sff.sh
@@ -0,0 +1,31 @@
+#
+# Copyright (C) 2025 Salvo Giangreco
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+# SEC Floating Feature configuration file for Galaxy A71 (a71)
+
+# Disable unsupported features
+SEC_FLOATING_FEATURE_GRAPHICS_SUPPORT_3D_SURFACE_TRANSITION_FLAG=
+SEC_FLOATING_FEATURE_LCD_CONFIG_HFR_SUPPORTED_REFRESH_RATE_NS=
+
+# Camera
+SEC_FLOATING_FEATURE_CAMERA_CONFIG_VENDOR_LIB_INFO=image_codec.samsung.v1,beauty.samsung.v4,face_landmark.arcsoft.v2_1,facial_attribute.samsung.v1,swuwdc.arcsoft.v1,event_detection.samsung.v2,food.samsung.v1,selfie_correction.samsung.v1,dual_bokeh.samsung.v1,single_bokeh.samsung.v2,image_enhance.arcsoft.v1,mfhdr.arcsoft.v1,llhdr.arcsoft.v1,scene_detection.samsung.v1,smart_scan.samsung.v2,human_tracking.arcsoft.v2_1,localtm.samsung.v1_1
+
+# Samsung Gallery app
+SEC_FLOATING_FEATURE_GALLERY_CONFIG_IMAGE_TAGGER_VERSION=V701
+
+# Always show remaster button in Gallery
+SEC_FLOATING_FEATURE_SAIV_CONFIG_AI_REVITAL_VERSION=1.7,1
